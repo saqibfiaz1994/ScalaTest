b3fe49d921aea90aa4688e7483fd3c5c79c79a28
Saqib Fiaz <saqib.fiaz@digital.hmrc.gov.uk>
2019-02-07 16:00:15 +0000

    final for the day

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -4,23 +4,30 @@ import scala.io.StdIn
 
 object GreeterApplication extends App {
 object Prompt {
+
+
   def ask(message: String) = StdIn.readLine(message)
+
 }
-  class Person (val name : String)
-  val nonames = Prompt.ask("what is your name?")
-  val p = new Person("george")
+  class Person (val name : String, age : Int)
+  private val	years	:	String	=	if(age	== 1)	"year" else "years"
 
-    def greet(name : String) :	Unit	=	{
 
-      if	(name	==	p.name)	{
+  val names = Prompt.ask("what is your name?")
+    val age = Prompt.ask("how old are you?").toInt
+    val p = new Person("george", age)
 
+  def greet(name : String) :	Unit	=	{
+
+      if	(name	==	p.name) {
         println("You don't get a hello!")
 
-      }	else	{
 
-        println(s"Hello $name")
+      } else	{
+
+        println(s"Hello $name, you are $age $years old")
       }
 
     }
-  greet(nonames)
+  greet(names)
 }
